9bf35277178f157e542dc896c53667b975eb3a63
Rishabh Rai <rishabh996633@gmail.com>
2025-03-01 12:14:58 +0530

    Fix environment variables for Vercel

--- vite.config.js
+++ vite.config.js
@@ -2,14 +2,11 @@ import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
 
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), ''); 
+export default defineConfig(() => {
+  process.env = { ...process.env, ...loadEnv(mode, process.cwd(), '') };
 
   return {
     plugins: [react(),tailwindcss()],
-    base: "/Chef-App",
-    define: {
-      'import.meta.env.VITE_HF_ACCESS_TOKEN': `"${env.VITE_HF_ACCESS_TOKEN}"`
-    }
+    base: "/", 
   };
 });
