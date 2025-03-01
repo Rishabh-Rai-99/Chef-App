f2892c039d48ac02eb147798eec72bd14f25616a
Rishabh Rai <rishabh996633@gmail.com>
2025-03-01 12:24:28 +0530

    Fix asset loading issue

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
 
-export default defineConfig(() => {
+export default defineConfig(({ mode }) => {
   process.env = { ...process.env, ...loadEnv(mode, process.cwd(), '') };
 
   return {
