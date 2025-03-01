a53030c60fe80bbd05d5fa87595283854e435cdc
Rishabh Rai <rishabh996633@gmail.com>
2025-03-01 11:48:29 +0530

    removed api key security

--- vite.config.js
+++ vite.config.js
@@ -6,8 +6,6 @@ export default defineConfig(({ mode }) => ({
   plugins: [react(), tailwindcss()],
   base: "/Chef-App",
   define: {
-    'import.meta.env.VITE_HF_ACCESS_TOKEN': mode === 'development' 
-      ? `"${process.env.VITE_HF_ACCESS_TOKEN}"` 
-      : `""`  // Empty in production
+    'import.meta.env.VITE_HF_ACCESS_TOKEN': `"your_api_key_here"`
   }
 }));
